3af16ba69e0f65a90b7f9b49c62fa8c6b12f192b
Volodymyr Hil <vh@pintle.dk>
2018-04-27 14:30:22 +0200

    sugdk initial commit

---
+++ gulp-config.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  var config = {
+	  websiteRoot: "D:\\Projects\\SUGDK\\Sugdk_20180508\\deploy\\local.sugdk901.com",
+    buildConfiguration: "Debug",
+    runCleanBuilds: false
+  }
+  return config;
+}
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/// <binding AfterBuild='Publish-Solution' />
+var gulp = require("gulp");
+var msbuild = require("gulp-msbuild");
+var debug = require("gulp-debug");
+var foreach = require("gulp-foreach");
+//var rename = require("gulp-rename");
+//var watch = require("gulp-watch");
+//var newer = require("gulp-newer");
+var runSequence = require("run-sequence");
+//var path = require("path");
+var config = require("./gulp-config.js")();
+//var nugetRestore = require('gulp-nuget-restore');
+//var fs = require('fs');
+
+module.exports.config = config;
+
+gulp.task("default", function (callback) {
+  config.runCleanBuilds = true;
+  return runSequence(
+    "Publish-Solution",
+	callback);
+});
+
+gulp.task("Publish-Solution", function () {
+	var dest = config.websiteRoot;
+	var targets = ["Build"];
+	if (config.runCleanBuilds) {
+		targets = ["Clean", "Build"];
+	}
+	var projects = ["./**/Sug*.csproj"];
+	console.log("publishing to '" + dest + "' folder");
+	return gulp.src(projects)
+	  .pipe(foreach(function (stream, file) {
+	  	return stream
+		  .pipe(debug({ title: "Building project:" }))
+		  .pipe(msbuild({
+		  	targets: targets,
+		  	configuration: config.buildConfiguration,
+		  	logCommand: false,
+		  	verbosity: "minimal",
+		  	maxcpucount: 0,
+		  	toolsVersion: 14.0,
+		  	properties: {
+		  		DeployOnBuild: "true",
+		  		DeployDefaultTarget: "WebPublish",
+		  		WebPublishMethod: "FileSystem",
+		  		DeleteExistingFiles: "false",
+		  		publishUrl: dest,
+		  		_FindDependencies: "false"
+		  	}
+		  }));
+	  }));
+});
